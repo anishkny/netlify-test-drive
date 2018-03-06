dca66cbdd685245ea5424e6b4e1e3111f981b2d2
Anish Karandikar <akarandi@akarandi-maci.dhcp.mathworks.com>
2018-03-06 16:00:24 -0500

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  head: {
+    titleTemplate: 'Netlify Test Drive',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ]
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Hello, world!</h1>
+  </div>
+</template>
+
+<style>
+body {
+  margin: 40px auto;
+  max-width: 650px;
+  line-height: 1.6;
+  font-size: 18px;
+  color: #444;
+  padding: 0 10px
+}
+
+h1, h2, h3 {
+  line-height: 1.2
+}
+</style>
